a0a33e4a7307d51fc36eba6755e2a765a3ce3a1f
Acrylic125 <acrylic125email@gmail.com>
2022-09-21 06:52:18 +0800

    feat: add proviers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "../styles/globals.css";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      {/* <UserProvider autoRefreshToken supabaseClient={supabaseClient}>
+        
+      </UserProvider> */}
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+// import React from "react";
+// import { UserProvider } from "@supabase/auth-helpers-react";
+// import { supabaseClient } from "@supabase/auth-helpers-nextjs";
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     // <UserProvider supabaseClient={supabaseClient}>
+//     <Component {...pageProps} />
+//     // </UserProvider>
+//   );
+// }
