f7bca39069c17836c38fcbb6e3261b980fd78775
Acrylic125 <acrylic125email@gmail.com>
2022-09-21 06:46:23 +0800

    feat: remove auth0

--- pages/api/auth/[...auth0].js
+++
@@ -1,5 +0,0 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
-
-// https://auth0.com/docs/quickstart/webapp/nextjs?a=cLgxKoF9UiQQzAl8ZlkWzWLeLFcFKATv&framed=1&sq=1#install-the-auth0-next-js-sdk
-// Creates the me, login, logout, and callback routes for you
-export default handleAuth();
