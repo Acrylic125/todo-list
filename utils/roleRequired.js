4ba19e91853e19424acc98bc7992e51e07425c53
Acrylic125 <acrylic125email@gmail.com>
2022-10-03 20:25:02 +0800

    feat: role protect util

---
+++ utils/roleRequired.js
@@ -0,0 +1,59 @@
+import { SupabaseClient, withPageAuth } from "@supabase/auth-helpers-nextjs";
+
+/**
+ * @param {string | string[]} roles
+ * @param {(ctx: import("next").GetServerSidePropsContext, supabaseClient: SupabaseClient) => Promise<import("next").GetServerSidePropsResult<P>>} whenNotRole
+ * @template P
+ * @returns
+ */
+const roleRequired = (roles, whenNotRole) => {
+  /** @type {import("./withPageAuthWrap").AuthWrapFunction} */
+  return async (ctx, supabaseClient) => {
+    const {
+      data: { user },
+      error: getUserError,
+    } = await supabaseClient.auth.getUser();
+    if (getUserError) {
+      throw getUserError;
+    }
+
+    const {
+      data: [userRole],
+      error: getUserRoleError,
+    } = await supabaseClient.from("user_roles").select("*").eq("user_id", user.id);
+    if (getUserRoleError) {
+      throw getUserRoleError;
+    }
+
+    var hasRole = roles instanceof Array ? roles.includes(userRole.role) : userRole.role === roles;
+
+    if (!hasRole) {
+      if (typeof whenNotRole === "function") {
+        const whenNotRoleResult = await whenNotRole(ctx, supabaseClient);
+
+        return {
+          terminate: true,
+          data: whenNotRoleResult,
+        };
+      }
+
+      return {
+        terminate: true,
+        data: {
+          redirect: {
+            statusCode: 302,
+            destination: "/",
+          },
+        },
+      };
+    }
+
+    return {
+      data: {
+        props: {},
+      },
+    };
+  };
+};
+
+export default roleRequired;
