a14840438a3a22d524323c70220fcea099efa22c
Acrylic125 <acrylic125email@gmail.com>
2022-10-06 09:53:42 +0800

    refactor: clean up

--- utils/banProtected.js
+++ utils/banProtected.js
@@ -9,45 +9,43 @@ import { SupabaseClient, withPageAuth } from "@supabase/auth-helpers-nextjs";
 const banProtected = (whenBanned) => {
   /** @type {import("./withPageAuthWrap").AuthWrapFunction} */
   return async (ctx, supabaseClient) => {
-    {
-      const {
-        data: { user },
-        error: getUserError,
-      } = await supabaseClient.auth.getUser();
-      if (getUserError) {
-        throw getUserError;
-      }
+    const {
+      data: { user },
+      error: getUserError,
+    } = await supabaseClient.auth.getUser();
+    if (getUserError) {
+      throw getUserError;
+    }
 
-      const { data: isBanned, error: isBannedError } = await supabaseClient.rpc("is_banned", {
-        uid: user.id,
-      });
-      if (isBannedError) {
-        throw isBannedError;
-      }
+    const { data: isBanned, error: isBannedError } = await supabaseClient.rpc("is_banned", {
+      uid: user.id,
+    });
+    if (isBannedError) {
+      throw isBannedError;
+    }
 
-      if (isBanned) {
-        if (typeof whenBanned === "function") {
-          const whenBannedResult = await whenBanned(ctx, supabaseClient, bans);
-          return {
-            terminate: true,
-            results: whenBannedResult,
-          };
-        }
+    if (isBanned) {
+      if (typeof whenBanned === "function") {
+        const whenBannedResult = await whenBanned(ctx, supabaseClient, bans);
         return {
           terminate: true,
-          results: {
-            redirect: {
-              permanent: true,
-              destination: "/ban",
-            },
-          },
+          results: whenBannedResult,
         };
       }
-
       return {
-        props: {},
+        terminate: true,
+        results: {
+          redirect: {
+            permanent: true,
+            destination: "/ban",
+          },
+        },
       };
     }
+
+    return {
+      props: {},
+    };
   };
 };
 
