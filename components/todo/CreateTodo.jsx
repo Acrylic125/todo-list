d9e6779d58a4cbe510a90d1d6e760359b327ec39
Acrylic125 <acrylic125email@gmail.com>
2022-10-29 13:15:40 +0800

    feat: daisyui create todo

--- components/todo/CreateTodo.jsx
+++ components/todo/CreateTodo.jsx
@@ -1,25 +1,108 @@
-import { Button, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import cx from "classnames";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
 
-export default function CreateTodo({ onRequestCreateTodo }) {
-  const form = useForm({
-    initialValues: {
-      todo: "",
+const CreateTodo = ({ className }) => {
+  const queryClient = useQueryClient();
+  const supabaseClient = useSupabaseClient();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, submitCount },
+  } = useForm();
+  const { mutate: createTodo } = useMutation(
+    async (newTodo) => {
+      const {
+        data: { user },
+        error: userError,
+      } = await supabaseClient.auth.getUser();
+      if (userError) {
+        throw new userError();
+      }
+      if (!user) {
+        throw new Error("No user found");
+      }
+
+      const { data, error } = await supabaseClient.from("todos").insert({ title: newTodo.title, user_id: user.id, due_on: newTodo.due_on });
+      if (error) throw new Error(error.message);
+      return data;
     },
-    validate: {
-      todo: (value) => (value.length > 0 ? null : "Please type out what you want to do"),
-    },
-  });
+    {
+      onMutate: async (newTodo) => {
+        // We are NOT cancelling the request to delete the todo, we are cancelling
+        // any requests to REFETCH the todos. We will always want to refetch the
+        // once after any set of mutations.
+        await queryClient.cancelQueries(["todos"]);
+        const previous = queryClient.getQueryData(["todos"]);
+
+        // console.log(newTodo);
+        queryClient.setQueryData(["todos"], (old) => {
+          return [
+            ...old,
+            {
+              id: Math.random(),
+              title: newTodo.title,
+              due_on: newTodo.due_on,
+              completed: false,
+              isLoading: true,
+            },
+          ];
+        });
+
+        return {
+          previous,
+        };
+      },
+      onError: (err, variables, context) => {
+        queryClient.setQueryData(["todos"], context.previous);
+      },
+      onSettled: (newTodo, error, variables, options) => {
+        // Refetch the todos after the mutation is done.
+        queryClient.invalidateQueries(["todos"]);
+      },
+    }
+  );
+
+  function submit(data) {
+    createTodo({
+      title: data.title,
+      due_on: data.due_on,
+    });
+  }
 
   return (
-    <form
-      onSubmit={form.onSubmit(({ todo }) => {
-        onRequestCreateTodo(todo);
-      })}
-      className="flex flex-row gap-4"
-    >
-      <TextInput className="w-full" placeholder="Add a todo" required {...form.getInputProps("todo")} />
-      <Button type="submit">Add</Button>
+    <form onSubmit={handleSubmit(submit)} className={className}>
+      <div className="flex flex-row gap-4">
+        <input
+          {...register("title", {
+            required: "Title is required",
+            maxLength: {
+              message: "Title must be less than 255 characters",
+              value: 255,
+            },
+          })}
+          type="text"
+          placeholder="Add a todo"
+          className={cx("input input-bordered flex-1", {
+            "input-error": errors.title !== undefined && submitCount > 0,
+          })}
+        />
+        <input
+          {...register("due_on", {
+            required: "Due date is required",
+          })}
+          type="date"
+          className={cx("input input-bordered", {
+            "input-error": errors.due_on !== undefined && submitCount > 0,
+          })}
+        />
+        <button type="submit" className="btn btn-primary">
+          Add Todo
+        </button>
+      </div>
     </form>
   );
-}
+};
+
+export default CreateTodo;
